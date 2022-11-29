55ef3c532930da1bfbce5c061d588b51509fd205
kw <kw@csii.com>
2022-11-29 22:50:04 +0800

    v20221129 计算两个字符串日期之差

--- src/main/java/com/example/salaryproject/day20221117/DateCommonUtils.java
+++ src/main/java/com/example/salaryproject/day20221117/DateCommonUtils.java
@@ -1,10 +1,15 @@
 package com.example.salaryproject.day20221117;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 /**
@@ -13,6 +18,7 @@ import java.util.Date;
  * @author: kangwei
  * @create: 2022-11-17 21:57
  **/
+@Slf4j
 public class DateCommonUtils {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DateCommonUtils.class);
@@ -48,9 +54,33 @@ public class DateCommonUtils {
             LOGGER.error("日期解析异常" + e);
         }
         return null;
+
     }
 
-    public static void main(String[] args) {
-        System.out.println(checkDate("2022-11-17"));
+    /**
+     * 计算两个时间字符串
+     *
+     * @param start
+     * @param end
+     */
+    public static void days(String start, String end) {
+        try {
+            if (StringUtils.isBlank(start) || StringUtils.isBlank(end)) {
+                return;
+            }
+
+            LocalDate startDate = LocalDate.parse(start, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            LocalDate endDate = LocalDate.parse(end, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+            long days = startDate.until(endDate, ChronoUnit.DAYS);
+            long years = startDate.until(endDate, ChronoUnit.YEARS);
+            long months = startDate.until(endDate, ChronoUnit.MONTHS);
+
+            log.info("两个日期相差天数==>{}", days);
+            log.info("两个日期相差年==>{}", years);
+            log.info("两个日期相差月==>{}", months);
+        } catch (Exception e) {
+            log.error("日期解析异常", e);
+        }
     }
 }
